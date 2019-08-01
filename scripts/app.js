c146e56cd2d726aeedc5906dd9ad4a976b604c54
Lawrence <lawrencema415@gmail.com>
2019-08-01 16:06:56 -0700

    checking eventlistener

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,8 @@
 console.log("hello");
 $('#mce-EMAIL').keydown( (e)=> {
     console.log("changing");
+      // mce-FNAME
+      // mce-LNAME
+      $fname = $('#mce_FNAME');
+      console.log($fname.val);
 })
