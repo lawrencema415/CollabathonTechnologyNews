3c132c38d1fc2fd9270975ebdb9b228c55a7a8cb
Lawrence <lawrencema415@gmail.com>
2019-08-02 10:23:08 -0700

    testing updates

--- scripts/app.js
+++ scripts/app.js
@@ -7,13 +7,17 @@ function validateEmail(email) {
 
 $('#mce-EMAIL').keydown( (e)=> {
     if($("#mce-FNAME").val() != "" && $('#mce-LNAME').val() != "" && $('#mce-EMAIL').val() != "" ) {
-      if(validateEmail($('#mce-EMAIL').val())) {
+      if(validateEmail($('#mce-EMAIL').val()) == true) {
+        console.log("validates");
         $("#mc-embedded-subscribe").attr("disabled",false);
-        // $("#mc-EMAIL").css('border-color', 'black');
+        if("#mc-EMAIL") {
+        	console.log("hello");
+        }
+        $("#mc-EMAIL").css('border', '2px solid green');
       }
     } else {
       $("#mc-embedded-subscribe").attr("disabled",true);
-      // $("#mc-EMAIL").css('border-color', 'red');
+      $("#mc-EMAIL").css('border', '2px solid red');
     }
 })
 
@@ -21,11 +25,11 @@ $('#mce-LNAME').keydown( (e)=> {
     if($("#mce-FNAME").val() != "" && $('#mce-LNAME').val() != "" && $('#mce-EMAIL').val() != "" ) {
       if($('#mce-LNAME').val().length < 2) {
         $("#mc-embedded-subscribe").attr("disabled",false);
-        // $("#mc-LNAME").css('border-color', 'black');
+        $("#mc-LNAME").css('border', '2px solid green');
       }
     } else {
       $("#mc-embedded-subscribe").attr("disabled",true);
-      // $("#mc-LNAME").css('border-color', 'red');
+      $("#mc-LNAME").css('border', '2px solid red');
     }
 })
 
@@ -33,10 +37,10 @@ $('#mce-FNAME').keydown( (e)=> {
     if($("#mce-FNAME").val() != "" && $('#mce-LNAME').val() != "" && $('#mce-EMAIL').val() != "" ) {
       if($('#mce-FNAME').val().length < 2) {
         $("#mc-embedded-subscribe").attr("disabled",false);
-        // $("#mc-FNAME").css('border-color', 'black');
+        $("#mc-FNAME").css('border', '2px solid green');
       }
     } else {
       $("#mc-embedded-subscribe").attr("disabled",true);
-      // $("#mc-FNAME").css('border-color', 'red');
+      $("#mc-FNAME").css('border', '2px solid red');
     }
 })
